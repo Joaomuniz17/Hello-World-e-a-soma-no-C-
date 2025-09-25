fbf44caee700c993347bbccb3a1c2bf47227f1a1
João Vitor Muniz Vasconcelos <140927075+Joaomuniz17@users.noreply.github.com>
2025-09-25 19:28:52 -0300

    Um testizin ae, aprendi no Youtube :)

---
+++ Hello World e a Soma.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // uma biblioteca para entrada e saída
+using namespace std;
+
+int main() {
+    // imprime Hello World
+    cout << "Hello, World!" << endl;
+
+    // faz uma soma padrão
+    int a = 5;
+    int b = 7;
+    int soma = a + b;
+
+    cout << "A soma de " << a << " + " << b << " = " << soma << endl;
+
+    return 0; // indica que terminou sem erros :D
+}
